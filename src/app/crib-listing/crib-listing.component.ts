0f1b2af038a262578d6e1bdbdbf83d7890d32131
ssinha <ssinha@kabbage.com>
2017-07-07 16:15:12 -0400

    added cribs list and bootstrap

--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {cribs} from './../data/cribs';
 @Component({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CribListingComponent implements OnInit {
 
+  cribs: Array<any> = cribs;
   constructor() { }
 
   ngOnInit() {
