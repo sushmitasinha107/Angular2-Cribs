a3c307229979191c4aef3af671f221414ec0557b
ssinha <ssinha@kabbage.com>
2017-07-07 14:40:27 -0400

    added component crib-listing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { HttpModule } from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
   ],
   imports: [
     BrowserModule
