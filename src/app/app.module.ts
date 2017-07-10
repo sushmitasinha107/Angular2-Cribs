c243f591b57f7f36dd533fa770ddc6c32e09bb4c
ssinha <ssinha@kabbage.com>
2017-07-10 12:37:12 -0400

    added data service for crib data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { HttpModule } from '@angular/http';
-
-
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
 import { CribCardComponent } from './crib-card/crib-card.component';
+import { CribsService } from "app/services/cribs.service";
 
 @NgModule({
   declarations: [
@@ -16,9 +14,9 @@ import { CribCardComponent } from './crib-card/crib-card.component';
     CribCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpModule
   ],
-  providers: [],
+  providers: [CribsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/crib-listing/crib-listing.component.ts
+++ src/app/crib-listing/crib-listing.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {cribs} from './../data/cribs';
+import { Http } from '@angular/http';
+import {CribsService} from './../services/cribs.service';
+import { Crib } from './../crib';
+
 @Component({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
@@ -7,10 +10,13 @@ import {cribs} from './../data/cribs';
 })
 export class CribListingComponent implements OnInit {
 
-  cribs: Array<any> = cribs;
-  constructor() { }
-
+  cribs: Array<any>;
+  error: string;
+  constructor(private http: Http, private cribsService: CribsService) {
+   }
   ngOnInit() {
-  }
+    this.cribsService.getAllCribs().subscribe(data => this.cribs = data );
+}
+
 
 }
