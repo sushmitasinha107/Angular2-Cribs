e45d0a9d7f124d99200a9cdfc6eaaf6b8aa69715
ssinha <ssinha@kabbage.com>
2017-07-10 11:01:32 -0400

    sdding component crib card

---
+++ src/app/crib-card/crib-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crib-card',
+  templateUrl: './crib-card.component.html',
+  styleUrls: ['./crib-card.component.css']
+})
+export class CribCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
